200a5f78e0f9dea32003ad8056bbe2405e8ee612
flameindark <895226083@qq.com>
2018-06-05 23:34:56 +0800

    06.05promise 原理的简易实现代码完成，理解。明天根据这个写测试

--- js/promise/index.js
+++ js/promise/index.js
@@ -90,3 +90,98 @@ function Promise3(fn) {
     fn(resolve)
 }
 // 06.05今日Promise链式调用的一部分代码没有理解通透
+
+// 失败的处理
+function Promise4(fn) {
+    var value = null,
+        state = 'pending',
+        callbacks = [];
+
+    this.then = function(onFulFilled, onRejected) {
+        return new Promise(function(resolve) {
+            handle( {
+                onFulFilled: onFulFilled || null,
+                onRejected: onRejected || null,
+                resolve: resolve,
+                reject: reject
+            }) // 将状态和回调函数传入，根据状态执行不同的回调
+        })
+    }
+    function handle(callback) {
+        if (state === 'pending') {
+            callbacks.push(callback);
+            return;
+        }
+        
+        var cb = state === 'fulfilled' ? callback.onFulFilled : callback.onRejected,
+        ret;
+        // 对这段代码进行相应的改写
+        // if(!callback.onFulFilled) {
+        //     callback.resolve(value);
+        //     return;
+        // }
+        if(cb === null) {
+            cb = state === 'fulfilled' ? callback.resolve : callback.reject;
+            cb(value);
+            return;
+        }
+
+        var ret = cb(value);
+        callback.resolve(ret);
+    }
+    function resolve(newVal) {
+        if(newVal && (typeof newVal === 'object' || typeof newVal === 'function')) {
+            var then = newVal.then;
+            if(typeof then === 'function') {
+                // 上面的逻辑判断是否有then方法以此来判断是否为Promise的链式调用
+                then.call(newVal, resolve);
+                // 执行新的promise的resolve方法
+                return;
+            }
+        }
+        value = newVal;
+        state = 'fulfilled';
+        
+    }
+    function reject(reject) {
+        state = 'rejected';
+        value = reason;
+        execute();
+    }   
+
+    function execute() {
+        setTimeout(function() {
+            callbacks.forEach(function(callback) {
+                handle(callback)
+            })
+        }, 0);
+    }
+
+    fn(resolve, reject)
+}
+
+// 异常处理（执行回调时代码报错，通过try-catch捕获，将状态设置成reject）
+// 做法就是改写上面的handle方法
+function handle(callback) {
+    if (state === 'pending') {
+        callbacks.push(callback);
+        return;
+    }
+    
+    var cb = state === 'fulfilled' ? callback.onFulFilled : callback.onRejected,
+    ret;
+    if(cb === null) {
+        cb = state === 'fulfilled' ? callback.resolve : callback.reject;
+        cb(value);
+        return;
+    }
+    // 将下面两行用try catch来做错误的处理，报错就调用reject方法
+    // var ret = cb(value);
+    // callback.resolve(ret);
+    try {
+        ret = cb(value);
+        callback.resolve(ret);
+    } catch (e) {
+        callback.reject(e);
+    } 
+}
