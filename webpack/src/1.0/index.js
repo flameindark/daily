f434b3ede69f0b069c270c7f1fe743b2a02c54ba
flameindark <895226083@qq.com>
2018-06-12 08:39:20 +0800

    测试重复引用模块是否重复打包

--- webpack/src/1.0/index.js
+++ webpack/src/1.0/index.js
@@ -1,7 +1,6 @@
 import './index.scss'
 import 'normalize-css'
 import './lodash-test1.js'
-import './lodash-test2.js'
 
 
 var oDiv = document.createElement('div');
---
+++ webpack/src/1.0/lodash-test2.1.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'
+
+var array = [1, 2, 3];
+ 
+_.fill(array, 'a');
+console.log(array);
+// => ['a', 'a', 'a']
+ 
+_.fill(Array(3), 2);
+// => [2, 2, 2]
+ 
+_.fill([4, 6, 8, 10], '*', 1, 3);
+
+console.log(array);
